ad59c4ef130d40c7fdf8b3c16fa13fbd95844538
wen2ms <1819509111@qq.com>
2025-06-27 01:35:22 +0800

    Feat: Input validates with QLineEdit

--- windows/login.cpp
+++ windows/login.cpp
@@ -5,52 +5,91 @@
 
 #include "ui_login.h"
 
-Login::Login(QWidget *parent) : QDialog(parent), ui(new Ui::Login) {
+Login::Login(QWidget* parent) : QDialog(parent), ui(new Ui::Login) {
     ui->setupUi(this);
-    
+
     ui->stackedWidget->setCurrentIndex(0);
-    
-    connect(ui->home_btn, &QPushButton::clicked, this, [=]() {
-        ui->stackedWidget->setCurrentIndex(0);
-    });
-    
-    connect(ui->register_user, &QPushButton::clicked, this, [=]() {
-        ui->stackedWidget->setCurrentIndex(1);
-    });
-    
-    connect(ui->net_config_btn, &QPushButton::clicked, this, [=]() {
-        ui->stackedWidget->setCurrentIndex(2);
-    });
-    
+
+    connect(ui->home_btn, &QPushButton::clicked, this, [=]() { ui->stackedWidget->setCurrentIndex(0); });
+
+    connect(ui->register_user, &QPushButton::clicked, this, [=]() { ui->stackedWidget->setCurrentIndex(1); });
+
+    connect(ui->net_config_btn, &QPushButton::clicked, this, [=]() { ui->stackedWidget->setCurrentIndex(2); });
+
     QRegularExpression expreg("^[a-zA-Z0-9_]{3,9}$");
     QRegularExpressionValidator* validator_name = new QRegularExpressionValidator(expreg, this);
-    
+
     ui->username->setValidator(validator_name);
     ui->reg_username->setValidator(validator_name);
-    
-    
+
     expreg.setPattern("^(?=.*[A-Z])(?=.*\\d)(?=.*[a-z])(?=.*[@$!%*?&_])[a-zA-Z\\d@$!%*?&_]{4,20}$");
     QRegularExpressionValidator* validator_pwd = new QRegularExpressionValidator(expreg, this);
-    
+
     ui->password->setValidator(validator_pwd);
     ui->reg_password->setValidator(validator_pwd);
-    
+
     expreg.setPattern("^1[3456789]\\d{9}$");
     QRegularExpressionValidator* validator_phone = new QRegularExpressionValidator(expreg, this);
-    
+
     ui->phone->setValidator(validator_phone);
-    
+
     expreg.setPattern("^((25[0-5]|2[0-4]\\d|[01]?\\d\\d?)\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d\\d?)$");
     QRegularExpressionValidator* validator_ip = new QRegularExpressionValidator(expreg, this);
-    
+
     ui->host_addr->setValidator(validator_ip);
-    
+
     expreg.setPattern("^(6553[0-5]|655[0-2]\\d|65[0-4]\\d{2}|6[0-4]\\d{3}|[1-9]\\d{0,3}|0)$");
     QRegularExpressionValidator* validator_port = new QRegularExpressionValidator(expreg, this);
-    
+
     ui->port->setValidator(validator_port);
+    
+    connect(ui->login_btn, &QPushButton::clicked, this, &Login::on_login_btn_clicked);
+    connect(ui->reg_btn, &QPushButton::clicked, this, &Login::on_reg_btn_clicked);
+    connect(ui->confirm_btn, &QPushButton::clicked, this, &Login::on_confirm_btn_clicked);
+    
 }
 
 Login::~Login() {
     delete ui;
+}
+
+bool Login::verfify_data(QLineEdit* edit) {
+    if (edit->hasAcceptableInput()) {
+        edit->setStyleSheet("none");
+        
+        return true;
+    } else {
+        edit->setStyleSheet("border: 2px solid red;");
+        
+        return false;
+    }
+}
+
+void Login::on_login_btn_clicked() {
+    bool valid_name = verfify_data(ui->username);
+    bool valid_pwd = verfify_data(ui->password);
+    
+    if (valid_name && valid_pwd) {
+        
+    }
+}
+
+void Login::on_reg_btn_clicked() {
+    bool valid_reg_name = verfify_data(ui->reg_username);
+    bool valid_reg_pwd = verfify_data(ui->reg_password);
+    bool valid_reg_phone = verfify_data(ui->phone);
+    
+    
+    if (valid_reg_name && valid_reg_pwd && valid_reg_phone) {
+        
+    }
+}
+
+void Login::on_confirm_btn_clicked() {
+    bool valid_host = verfify_data(ui->host_addr);
+    bool valid_port = verfify_data(ui->port);
+    
+    if (valid_host && valid_port) {
+        
+    }
 }
--- windows/login.h
+++ windows/login.h
@@ -2,6 +2,7 @@
 #define LOGIN_H
 
 #include <QDialog>
+#include <QLineEdit>
 
 namespace Ui {
 class Login;
@@ -11,11 +12,20 @@ class Login : public QDialog {
     Q_OBJECT
 
   public:
-    explicit Login(QWidget *parent = nullptr);
+    explicit Login(QWidget* parent = nullptr);
     ~Login();
+
+    bool verfify_data(QLineEdit* edit);
+    
+  private slots:
+    void on_login_btn_clicked();
+    
+    void on_reg_btn_clicked();
+    
+    void on_confirm_btn_clicked();
     
   private:
-    Ui::Login *ui;
+    Ui::Login* ui;
 };
 
 #endif  // LOGIN_H
