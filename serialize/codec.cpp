88853391d0050f81249f86256a8ae370d1dcabfd
wen2ms <1819509111@qq.com>
2025-06-30 21:18:46 +0800

    Feat: Serialization and Deserialization

---
+++ serialize/codec.cpp
@@ -0,0 +1,49 @@
+#include "codec.h"
+
+Codec::Codec(Message *msg) {
+    reload(msg);
+}
+
+Codec::Codec(QByteArray msg) {
+    reload(msg);
+}
+
+QByteArray Codec::encode_msg() {
+    std::string out;
+    
+    obj_.SerializeToString(&out);
+    
+    return QByteArray::fromStdString(out);
+}
+
+QSharedPointer<Message> Codec::decode_msg() {
+    std::string data = msg_.toStdString();
+    
+    obj_.ParseFromString(data);
+    
+    Message* msg = new Message;
+    
+    msg->user_name = QByteArray::fromStdString(obj_.user_name());
+    msg->data1 = QByteArray::fromStdString(obj_.data1());
+    msg->data2 = QByteArray::fromStdString(obj_.data2());
+    msg->data3 = QByteArray::fromStdString(obj_.data3());
+    msg->reqcode = obj_.reqcode();
+    msg->rescode = obj_.rescode();
+    
+    QSharedPointer<Message> ptr(msg);
+    
+    return ptr;
+}
+
+void Codec::reload(QByteArray msg) {
+    msg_ = msg;
+}
+
+void Codec::reload(Message *msg) {
+    obj_.set_user_name(msg->user_name.toStdString());
+    obj_.set_data1(msg->data1.toStdString());
+    obj_.set_data2(msg->data2.toStdString());
+    obj_.set_data3(msg->data3.toStdString());
+    obj_.set_reqcode(msg->reqcode);
+    obj_.set_rescode(msg->rescode);
+}
---
+++ serialize/codec.h
@@ -0,0 +1,36 @@
+#ifndef CODEC_H
+#define CODEC_H
+
+#include <QByteArray>
+#include <QSharedPointer>
+
+#include "information.pb.h"
+
+struct Message {
+    QByteArray user_name;
+    QByteArray data1;
+    QByteArray data2;
+    QByteArray data3;
+    RequestCode reqcode;
+    ResponseCode rescode;
+};
+
+class Codec {
+  public:
+    Codec(Message* msg);
+    
+    Codec(QByteArray msg);
+    
+    QByteArray encode_msg();
+    
+    QSharedPointer<Message> decode_msg();
+    
+    void reload(QByteArray msg);
+    void reload(Message* msg);
+    
+  private:
+    QByteArray msg_;
+    Information obj_;
+};
+
+#endif  // CODEC_H
